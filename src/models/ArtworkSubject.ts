432469292dfde229d4eaf35a2f83f8fd9aafc3c5
Eric Jay Catayas <catayasericjay@gmail.com>
2025-11-03 10:09:40 +0800

    chore: remove Artwork Subject

--- src/models/ArtworkSubject.ts
+++
@@ -1,4 +0,0 @@
-export interface ArtworkSubject {
-  artworkId: number;
-  subjectId: number;
-}
--- src/models/Subject.ts
+++
@@ -1,4 +0,0 @@
-export interface Subject {
-  id: number;
-  name: string; // e.g., "Landscapes", "Seascapes", "Cityscapes"
-}
