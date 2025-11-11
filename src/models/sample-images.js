000ea41045a38572dac8011dfc9747a255fca359
EricCatayas <catayasericjay@gmail.com>
2025-11-11 16:41:20 +0800

    feat: sample images

---
+++ src/models/sample-images.js
@@ -0,0 +1,12 @@
+const sample_images = [
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-1x1.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-2x1.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-2x3.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-3x2.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-3x4.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-4x3.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-4x5.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-5x4.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-5x7.png",
+  "https://unlimitedworks.blob.core.windows.net/up-cebu-exchange/placeholder-img-7x5.png",
+];
