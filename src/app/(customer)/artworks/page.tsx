aac63a120927b6c0601ad7ec0ea5ac18f55ce922
EricCatayas <catayasericjay@gmail.com>
2025-11-11 16:45:37 +0800

    style: increase artwork image max width

--- src/app/(customer)/artworks/page.tsx
+++ src/app/(customer)/artworks/page.tsx
@@ -18,7 +18,7 @@ function Artworks() {
       <button>Latest</button>
       <br />
       {/* Line break here */}
-      <div className="columns-1 md:columns-2 lg:columns-3 gap-2 mt-6 space-y-6">
+      <div className="columns-1 md:columns-2 lg:columns-3 gap-6 mt-6 space-y-6">
         {allArtworks.map((artwork) => (
           <div key={artwork.id} className="break-inside-avoid mb-6">
             <ArtworkCard artwork={artwork} />
