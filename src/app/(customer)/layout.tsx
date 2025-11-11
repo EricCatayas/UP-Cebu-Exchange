99da59428b804070bdecbd4f76e3f460fbb4f159
EricCatayas <catayasericjay@gmail.com>
2025-11-11 08:49:42 +0800

    update: moved footer and navbar in customer folder

--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "@/components/customer/Navbar";
+import Footer from "@/components/customer/Footer";
 
 export default function CustomerLayout({
   children,
--- src/components/customer/Navbar.tsx
+++ src/components/customer/Navbar.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white shadow-md">
+    <nav>
       <div className="container mx-auto px-4 py-2 flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <Link href="/">
