66cb99ac50cebe0687283b9da1ffc552c45037e1
EricCatayas <catayasericjay@gmail.com>
2025-11-11 10:22:52 +0800

    style: Navbar background transparent

--- src/components/customer/Navbar.tsx
+++ src/components/customer/Navbar.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
+    <nav className="bg-transparent">
       <div className="container mx-auto px-4 py-2 flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <Link href="/">
@@ -17,19 +17,19 @@ const Navbar: React.FC = () => {
               className="h-12 w-12 max-w-12"
             />
           </Link>
-          <h1 className="text-2xl font-bold text-gray-800">UP Cebu Exchange</h1>
+          <h1 className="text-2xl font-bold text-black">UP Cebu Exchange</h1>
         </div>
         <div className="flex items-center space-x-4">
-          <Link href="/cart" className="text-gray-600 hover:text-gray-800">
+          <Link href="/cart" className="text-black hover:text-gray-700">
             Cart
           </Link>
           <Link
             href="/account/rentals"
-            className="text-gray-600 hover:text-gray-800"
+            className="text-black hover:text-gray-700"
           >
             My Rentals
           </Link>
-          <Link href="/account" className="text-gray-600 hover:text-gray-800">
+          <Link href="/account" className="text-black hover:text-gray-700">
             Account
           </Link>
         </div>
