f62fe1edae2b6e69586e35f63aba53ca160c1d9f
EricCatayas <catayasericjay@gmail.com>
2025-11-12 08:42:46 +0800

    feat: contants file for global values

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const DELIVERY_FEE = 50;
+
+export const DELIVERY_METHODS = ['Delivery', 'Pickup'];
+
+export const DURATION_OPTIONS = [3, 6, 12];
+
+export const PAYMENT_METHODS = ['Credit Card', 'PayPal', 'Bank Transfer'];
