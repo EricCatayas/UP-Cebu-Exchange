02dd31f7254cdddc24e1246158b232407116a033
EricCatayas <catayasericjay@gmail.com>
2025-11-17 14:56:35 +0800

    feat: WishlistService

---
+++ src/services/WishlistService.ts
@@ -0,0 +1,66 @@
+import { Artwork, Wishlist, WishlistItem, RentalPlan } from '@/models/sequelize';
+
+class WishlistService {
+  async addItem(userId: number, artworkId: number) {
+    let wishlist = await Wishlist.findOne({ where: { userId } });
+    if (!wishlist) {
+      wishlist = await Wishlist.create({ userId });
+    }
+    let wishlistItem = await WishlistItem.findOne({
+      where: { wishlistId: wishlist.id, artworkId },
+    });
+    if (!wishlistItem) {
+      await WishlistItem.create({ wishlistId: wishlist.id, artworkId });
+    }
+  }
+
+  async removeItem(userId: number, artworkId: number) {
+    const wishlist = await Wishlist.findOne({ where: { userId } });
+    if (!wishlist) {
+      throw new Error('Wishlist not found for user');
+    }
+    const wishlistItem = await WishlistItem.findOne({
+      where: { wishlistId: wishlist.id, artworkId },
+    });
+    if (!wishlistItem) {
+      throw new Error('Item not found in wishlist');
+    }
+    await wishlistItem.destroy();
+  }
+
+  async isItemInWishlist(userId: number, artworkId: number) {
+    const wishlist = await Wishlist.findOne({ where: { userId } });
+    if (!wishlist) {
+      return false;
+    }
+    const wishlistItem = await WishlistItem.findOne({
+      where: { wishlistId: wishlist.id, artworkId },
+    });
+    return !!wishlistItem;
+  }
+
+  async getWishlistItems(userId: number) {
+    const wishlist = await Wishlist.findOne({
+      where: { userId },
+      include: [
+        {
+          model: WishlistItem,
+          as: 'wishlistItems',
+          include: [
+            {
+              model: Artwork,
+              as: 'artwork',
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!wishlist) {
+      return [];
+    }
+    return wishlist.wishlistItems.map((item) => item.toJSON());
+  }
+}
+
+export default new WishlistService();
