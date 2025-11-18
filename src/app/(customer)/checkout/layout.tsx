f17bd769458134837c0ebb99cdd6ac9592a60e76
EricCatayas <catayasericjay@gmail.com>
2025-11-18 20:06:53 +0800

    feat: integrate UserAddressContext

--- src/app/(customer)/checkout/layout.tsx
+++ src/app/(customer)/checkout/layout.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { CartProvider } from '@/contexts/CartContext';
 import { RentalOrderProvider } from '@/contexts/RentalOrderContext';
+import { UserAddressProvider } from '@/contexts/UserAddressContext';
 import CartService from '@/services/CartService';
+import AddressService from '@/services/AddressService';
 import { getCurrentUser } from '@/lib/auth';
 
 export default async function CheckoutLayout({ children }: { children: React.ReactNode }) {
   const currentUser = await getCurrentUser();
-  const initialCartItems = currentUser ? await CartService.getCartItems(currentUser.userId) : [];
-  console.log('Initial Cart Items:', initialCartItems);
+  const userCartItems = currentUser ? await CartService.getCartItems(currentUser.userId) : [];
+  const userAddress = currentUser ? await AddressService.getUserAddress(currentUser.userId) : null;
 
   return (
     <>
-      <CartProvider initialCartItems={initialCartItems}>
-        <RentalOrderProvider>{children}</RentalOrderProvider>
+      <CartProvider initialCartItems={userCartItems}>
+        <RentalOrderProvider>
+          <UserAddressProvider userAddress={userAddress}>{children}</UserAddressProvider>
+        </RentalOrderProvider>
       </CartProvider>
     </>
   );
