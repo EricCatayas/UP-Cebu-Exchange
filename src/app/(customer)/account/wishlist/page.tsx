5121c7348e0d5462f1c1459199acea2d35471ade
EricCatayas <catayasericjay@gmail.com>
2025-11-18 08:54:42 +0800

    feat: My Wishlist page initial UI

--- src/app/(customer)/account/wishlist/page.tsx
+++ src/app/(customer)/account/wishlist/page.tsx
@@ -1,8 +1,22 @@
-// My Wishlist
-import React from "react";
+import React from 'react';
+import ArtworkGrid from '@/components/ArtworkGrid/ArtworkGrid';
+import ArtworkService from '@/services/ArtworkService';
+import PageHeader from '@/components/PageHeader/PageHeader';
+import { getCurrentUser } from '@/lib/auth';
 
-function Wishlist() {
-  return <div>Wishlist</div>;
+async function Artworks() {
+  const currentUser = await getCurrentUser();
+  const artworkService = new ArtworkService(currentUser?.userId);
+  const userWishlistArtworks = await artworkService.getUserWishlistArtworks();
+
+  return (
+    <>
+      <PageHeader title="My Wishlist" />
+      <div className="container mx-auto px-4 py-8">
+        <ArtworkGrid artworks={userWishlistArtworks} />
+      </div>
+    </>
+  );
 }
 
-export default Wishlist;
+export default Artworks;
