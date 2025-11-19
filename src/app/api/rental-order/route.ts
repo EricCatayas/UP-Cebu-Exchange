847be5e1a144290a8bf3f3cd9761e2ebc2b1df25
EricCatayas <catayasericjay@gmail.com>
2025-11-19 15:42:33 +0800

    update: rental order api return 400 response if address is not provided for delivery method orders

--- src/app/api/rental-order/route.ts
+++ src/app/api/rental-order/route.ts
@@ -1,8 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { Cart, CartItem, RentalOrder, RentalOrderItem, Payment, Artwork, RentalPlan } from '@/models/sequelize';
+import {
+  Address,
+  Artwork,
+  Cart,
+  CartItem,
+  RentalOrder,
+  RentalOrderItem,
+  Payment,
+  RentalPlan,
+} from '@/models/sequelize';
 import { RentalOrderCreateDTO } from '@/models/RentalOrder';
 import { getCurrentUser } from '@/lib/auth';
 import { getRentalFee } from '@/lib/artwork';
+import { DELIVERY_METHOD } from '@/lib/constants';
 
 export async function POST(request: NextRequest) {
   try {
@@ -18,6 +28,7 @@ export async function POST(request: NextRequest) {
       deliveryMethod,
       paymentMethod,
       cartItemIds,
+      addressId,
     }: RentalOrderCreateDTO = await request.json();
     const cart = await Cart.findOne({
       where: {
@@ -26,6 +37,27 @@ export async function POST(request: NextRequest) {
     });
     if (!cart) {
       return NextResponse.json({ error: 'Cart not found' }, { status: 404 });
+    }
+    if (deliveryMethod === DELIVERY_METHOD.DELIVERY && !addressId) {
+      return NextResponse.json({ error: 'Address is required for delivery method' }, { status: 400 });
+    }
+    let newAddressId = addressId;
+    if (addressId) {
+      // Create new copy of address
+      const existingAddress = await Address.findOne({
+        where: { id: addressId },
+      });
+      if (!existingAddress) {
+        return NextResponse.json({ error: 'Address not found' }, { status: 404 });
+      }
+      const newAddress = await Address.create({
+        city: existingAddress.city,
+        province: existingAddress.province,
+        postalCode: existingAddress.postalCode,
+        addressLine1: existingAddress.addressLine1,
+        addressLine2: existingAddress.addressLine2,
+      });
+      newAddressId = newAddress.id;
     }
     // Create Payment here
     const newPayment = await Payment.create({
@@ -42,6 +74,7 @@ export async function POST(request: NextRequest) {
       endDate: new Date(endDate),
       deliveryMethod,
       durationMonths: durationMonths,
+      addressId: newAddressId,
       status: 'Pending',
     });
 
@@ -71,6 +104,7 @@ export async function POST(request: NextRequest) {
         amount: getRentalFee(cartItem.artwork, durationMonths),
       });
     }
+
     // Clear cart items
     await CartItem.destroy({
       where: {
