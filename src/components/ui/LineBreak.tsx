661100f338a237c8e9fc25827efe84fae01132b4
EricCatayas <catayasericjay@gmail.com>
2025-11-28 09:44:40 +0800

    feat: line break component

---
+++ src/components/ui/LineBreak.tsx
@@ -0,0 +1,7 @@
+export default function LineBreak() {
+  return (
+    <div className="w-screen relative left-1/2 right-1/2 -mx-[50vw]">
+      <hr className="border-t border-gray-300" />
+    </div>
+  );
+}
