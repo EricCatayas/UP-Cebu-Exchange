3338a1eadb70dab7bf87c7596b6eb52bee5bbcf7
EricCatayas <catayasericjay@gmail.com>
2026-01-09 11:06:09 +0800

    fix: useEffect instead of useMemo for handling side effects

--- src/components/ArtworksFilterBar/ArtworksFilterBar.tsx
+++ src/components/ArtworksFilterBar/ArtworksFilterBar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { eventApi } from '@/lib/api/event';
 import { StyleDTO } from '@/models/Style';
@@ -88,11 +88,11 @@ export default function ArtworksFilterBar({ mediums, styles }: { mediums: string
     router.push('/artworks', { scroll: false });
   };
 
-  useMemo(() => {
+  useEffect(() => {
     applyFilter();
   }, [sortBy, selectedStyleIds, selectedMediums]);
 
-  useMemo(() => {
+  useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       applyFilter();
     }, 500);
