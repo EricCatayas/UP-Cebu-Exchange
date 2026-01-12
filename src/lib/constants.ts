28f424997471744c37387b620ab4475caba74267
EricCatayas <catayasericjay@gmail.com>
2026-01-12 18:58:46 +0800

    update: app support email

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = 'UP Cebu Exchange';
 export const APP_EMAIL = process.env.APP_EMAIL;
-export const APP_SUPPORT_EMAIL = 'support@upcebu.exchange'; // todo
+export const APP_SUPPORT_EMAIL = 'cebuinit.upcebu@up.edu.ph';
 
 export enum ARTWORK_MEDIUM {
   OIL_ON_CANVAS = 'Oil on canvas',
