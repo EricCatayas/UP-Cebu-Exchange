4ff8eb46983d65f7235c0fff1f363fbbfe73cad4
EricCatayas <catayasericjay@gmail.com>
2026-03-09 18:56:18 +0800

    style: set to receive status green color

--- src/lib/labels.ts
+++ src/lib/labels.ts
@@ -20,7 +20,7 @@ export const orderStatus = {
   [ORDER_STATUS.TORECEIVE]: {
     label: 'To Receive',
     value: ORDER_STATUS.TORECEIVE,
-    color: 'bg-indigo-100 text-indigo-800',
+    color: 'bg-green-100 text-green-800',
   },
   ['To Receive Overdue']: { label: 'Overdue Receive', value: 'Overdue Receive', color: 'bg-red-100 text-red-800' },
   [ORDER_STATUS.ONGOING]: { label: 'Ongoing', value: ORDER_STATUS.ONGOING, color: 'bg-purple-100 text-purple-800' },
